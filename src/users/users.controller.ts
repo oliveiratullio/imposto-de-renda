613a9f545d22c82486cae20a2a9f0acddefc0181
Túllio Oliveira <105726160+oliveiratullio@users.noreply.github.com>
2025-01-21 00:50:50 -0300

    fixed session create

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,55 +1,52 @@
 import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto, CreateUserSchema } from './dto/create-user.dto';
-import { LoginUserDto, LoginUserSchema } from './dto/login-user.dto';
-import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 import { ConfigService } from '@nestjs/config';
 
 @Controller('users')
 export class UsersController {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService,
     private configService: ConfigService,
   ) {}
 
   @Post('signup')
   async signUp(@Body() body: CreateUserDto) {
-    const result = CreateUserSchema.safeParse(body);
-    if (!result.success) {
-      throw new BadRequestException(result.error.format());
-    }
+    const user = await this.usersService.create(body);
+    const jwtSecret = this.configService.get<string>('JWT_SECRET');
+    const session = await this.usersService.createSession(user.id, jwtSecret);
 
-    return this.usersService.create(result.data as CreateUserDto);
+    return {
+      user: {
+        email: user.email,
+        name: user.name,
+        id: user.id,
+        access_token: session.accessToken,
+      },
+    };
   }
 
   @Post('login')
   async login(@Body() body: LoginUserDto) {
-    const result = LoginUserSchema.safeParse(body);
-    if (!result.success) {
-      throw new BadRequestException(result.error.format());
-    }
-
-    const { email, password } = result.data;
-    const user = await this.usersService.findOneByEmail(email);
-
+    const user = await this.usersService.findOneByEmail(body.email);
     if (
       !user ||
-      !(await this.usersService.comparePassword(password, user.password))
+      !(await this.usersService.comparePassword(body.password, user.password))
     ) {
       throw new BadRequestException('Invalid email or password');
     }
-    const jwtSecret = this.configService.get('JWT_SECRET');
-    const payload = { sub: user.id };
-    const access_token = this.jwtService.sign(payload, { secret: jwtSecret });
-    const userData = {
-      email: user.email,
-      name: user.name,
-      id: user.id,
-      access_token,
-    };
+
+    const jwtSecret = this.configService.get<string>('JWT_SECRET');
+    const session = await this.usersService.createSession(user.id, jwtSecret);
+
     return {
-      user: userData,
+      user: {
+        email: user.email,
+        name: user.name,
+        id: user.id,
+        access_token: session.accessToken,
+      },
     };
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,16 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+// users.service.ts
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService, // Injetar JwtService para geração do token
+  ) {}
 
   async create(createUserDto: CreateUserDto) {
     const { name, email, password } = createUserDto;
@@ -25,5 +30,17 @@ export class UsersService {
 
   async comparePassword(password: string, hashedPassword: string) {
     return await bcrypt.compare(password, hashedPassword);
+  }
+
+  async createSession(userId: string, jwtSecret: string) {
+    const payload = { sub: userId };
+    const access_token = this.jwtService.sign(payload, { secret: jwtSecret });
+    return this.prisma.session.create({
+      data: {
+        userId,
+        accessToken: access_token,
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+      },
+    });
   }
 }
